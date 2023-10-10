b9b0cf90f261595fe7aeca9f7a9aeeb2ef4344bc
Catalina Rivas <catalinarivas.iccp@gmail.com>
2023-10-10 12:43:54 -0300

    tablero

--- Proyectos/tablero_ajedres/server.py
+++ Proyectos/tablero_ajedres/server.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
     return render_template("index.html",row=8,col=8,color_one='red',color_two='black')
